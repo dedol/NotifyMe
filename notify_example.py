17c8656be08e22831afd574238dc95340379997b
dedol <dedol@ro.ru>
2020-01-18 11:29:37 +0300

    add

---
+++ notify_example.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+def notify(text):
+	user = 330303423
+	token = '557bf448d4b15bd86a5e08a0a8c705d9'
+	r = requests.get('http://notify.dedol.ru/send', params={'user': user, 
+															'token': token, 
+															'text': text})
+	if r.json()['status'] == 'ok':
+		return True
+	return False
+
+
+notify('Hello from Python!')
